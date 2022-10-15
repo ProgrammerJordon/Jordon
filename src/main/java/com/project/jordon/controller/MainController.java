526e70575c7e908f8cf3d1b2ea123fca63a15bd7
jordon <jordon.programming@gmail.com>
2022-10-15 09:48:47 +0900

    project initial setting

---
+++ src/main/java/com/project/jordon/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.project.jordon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/findo")
+    public String index() {
+        return "findo";
+    }
+}
