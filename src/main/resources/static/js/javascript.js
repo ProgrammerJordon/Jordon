3214e7c74d07f949d2df78606cc546cf3a907e18
jordon <jordon.programming@gmail.com>
2022-10-17 00:43:39 +0900

    front work include work

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -1,3 +1,27 @@
 function sidemenu() {
     alert('javascript');
 }
+
+function checkAll() {
+    if($("#cboxAll").is(':checked')) {
+        $("input[name=cbox]").prop("checked", true);
+    } else {
+        $("input[name=cbox]").prop("checked", false);
+    }
+}
+
+$(document).on("click", "input:checkbox[name=cbox]", function(e) {
+    let chks = document.getElementsByName("cbox");
+    let chksChecked = 0;
+    for(let i=0; i<chks.length; i++) {
+        let cbox = chks[i];
+        if(cbox.checked) {
+            chksChecked++;
+        }
+    }
+    if(chks.length == chksChecked){
+        $("#cboxAll").prop("checked", true);
+    }else{
+        $("#cboxAll").prop("checked",false);
+    }
+});
